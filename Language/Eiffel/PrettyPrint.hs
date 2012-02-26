8594e21d98733b8f805350c1f350cdc44b834b1f
Scott West <scott.gregory.west@gmail.com>
2012-02-26 23:36:26 +0100

    Fixing precedence of symbolic operators.

--- Language/Eiffel/PrettyPrint.hs
+++ Language/Eiffel/PrettyPrint.hs
@@ -276,7 +276,8 @@ expr' _ (PrecursorCall cname es) = text "Precursor" <+> maybe empty (braces . te
 expr' i (UnOpExpr uop e) = condParens (i > 12) $ text (unop uop) <+> exprPrec 12 e
 expr' i (BinOpExpr (SymbolOp op) e1 e2)
   | op == "[]" = exprPrec i e1 <+> brackets (expr e2)
-  | otherwise =  condParens (i > 1) (exprPrec i e1 <+> text op <+> exprPrec (i + 1) e2)
+  | otherwise =  condParens (i > 11) 
+                 (exprPrec 11 e1 <+> text op <+> exprPrec 12 e2)
 expr' i (BinOpExpr bop e1 e2) = 
   condParens (i > p) 
              (exprPrec lp e1 <+> text op <+> exprPrec rp e2)
