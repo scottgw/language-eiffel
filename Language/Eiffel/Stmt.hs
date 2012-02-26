464041ccfd7ed99c8bfa791d90c1ebd6726aeea7
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2012-02-26 19:55:19 +0100

    Added missing show definitions for statements

--- Language/Eiffel/Stmt.hs
+++ Language/Eiffel/Stmt.hs
@@ -38,20 +38,28 @@ instance Show a => Show (AbsStmt a) where
          "then ", show s1, "\n",
          "else ", show s2, "\n"
         ]
+    show (Inspect i cases def) = "inspect " ++ show i 
+        ++ concat (map showCase cases)
+        ++ showDefault def
     show (Check cs) = "check " ++ show cs ++ " end"
     show (Create t trg fName args) = 
         concat ["create ",braced t,show trg,".",fName,show args]
     show (DefCreate t e) = "create(def) " ++ braced t ++ show e
     show (CallStmt e) = show e
     show (Assign i e) = show i ++ " := " ++ show e ++ "\n"
+    show (AssignAttempt i e) = show i ++ " ?= " ++ show e ++ "\n"
     show (Print e) = "Printing: " ++ show e ++ "\n"
     show (PrintD e) = "PrintingD: " ++ show e ++ "\n"
     show (Loop fr _ un l) = "from " ++ show fr ++ " until " ++ show un ++
                           " loop " ++ show l ++ "end\n"
     show (Malloc s) = "Malloc: " ++ show s
+    show (Debug str stmt) = "debug (" ++ str ++ ")\n" ++ show stmt ++ "end\n"
     show BuiltIn = "built_in"
   
 braced t = case t of
   Nothing -> ""
   Just t' -> "{" ++ show t' ++ "}"
   
+showCase (l, s) = "when " ++ show l ++ " then\n" ++ show s
+showDefault Nothing = ""
+showDefault (Just s) = "else\n" ++ show s
