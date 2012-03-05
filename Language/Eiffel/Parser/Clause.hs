3f76c801efcd530ccbaba7c27466b6fde394eb83
Scott West <scott.gregory.west@gmail.com>
2012-03-05 09:03:19 +0100

    Parse empty assertion clauses.

--- Language/Eiffel/Parser/Clause.hs
+++ Language/Eiffel/Parser/Clause.hs
@@ -10,6 +10,13 @@ import Language.Eiffel.Parser.Lex
 import Text.Parsec
 
 clause :: Parser (Clause Expr)
-clause = do 
-  tag <- try (Just <$> identifier <* colon) <|> return Nothing
-  Clause tag <$> expr
+clause = do
+  let tagNoExpr = do
+        i <- identifier <* colon
+        p <- getPosition
+        e <- option (attachPos p (LitBool True)) expr
+        return (Clause (Just i) e)
+      withExpr = do
+        tag <- try (Just <$> identifier <* colon) <|> return Nothing
+        Clause tag <$> expr
+    in try tagNoExpr <|> withExpr
